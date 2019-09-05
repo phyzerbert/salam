615cca33aa05a209502a18a48667b67e7959f06a
phyzerbert <xian1017@outlook.com>
2019-09-06 03:55:57 +0800

    buyer

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -5,17 +5,14 @@
 <div class="br-logo"><a href="{{route('home')}}" class="mx-auto"><span>{{config('app.name')}}</span></a></div>
 <div class="br-sideleft overflow-y-auto">
     <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
-    <div class="br-sideleft-menu">
-        
-        @if($role == 'admin') 
+    <div class="br-sideleft-menu">            
+        @if ($role != 'buyer')
             <a href="{{route('home')}}" class="br-menu-link @if($page == 'home') active show-sub @endif">
                 <div class="br-menu-item">
                     <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
                     <span class="menu-item-label op-lg-0-force d-lg-none op-lg-0-force d-lg-none">{{__('page.dashboard')}}</span>
                 </div>
             </a>
-        @endif
-        @if ($role != 'buyer')
             {{-- Purchase --}}
             @php
                 $purchase_items = ['purchase', 'purchase_list', 'purchase_create'];
